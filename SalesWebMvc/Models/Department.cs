95a5e6a2e142decde5c3e2d48078686a1a00bf8b
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2019-08-20 08:50:16 -0300

    Crud Scalfoding

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace SalesWebMvc.Models
     public class Department
 
     {
+        [Key]
         public  int Iid { get; set; }
         public string  Name { get; set; }
     }
